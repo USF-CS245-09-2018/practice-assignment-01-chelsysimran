f9b0a53010dfe5ad04040bd1c6be098e85c5938d
chelsysimran <chels.simran@gmail.com>
2018-08-31 13:17:50 -0700

    Add files via upload

---
+++ FactorialIterative.java
@@ -0,0 +1,17 @@
+public class FactorialIterative implements Factorial {
+	
+	public int factorial(int n)
+	{
+		if (n>0)
+		{
+			int total = 1;
+			for (int i = n; i > 0; i++)
+			{
+				total *= i;
+				return total;
+				
+			}
+		}
+	}
+
+}
---
+++ FactorialRecursive.java
@@ -0,0 +1,18 @@
+
+public class FactorialRecursive implements Factorial {
+	
+	public int factorial(int n)
+	{
+		if (n == 0)
+		{
+			return 1;
+		}
+		
+		else
+		{
+			return n*factorial(n-1);
+		}
+		
+	}
+
+}
